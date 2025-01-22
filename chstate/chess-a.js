2150267d43de6108eae71f1ccc5813ca1872c799
Stej Project <belajarbroedu@gmail.com>
2025-01-22 11:47:41 +0700

    Update chess-a.js

--- chstate/chess-a.js
+++ chstate/chess-a.js
@@ -1184,12 +1184,6 @@ window.onload = function () {
             setTimeout(function () {
                 values = ['r', 'n', 'b', 'q', 'k', 'b', 'n', 'r', 'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 't', 'm', 'v', 'w', 'l', 'v', 'm', 't'];
             }, 100);
-        } elseif (bestEffect = 100) {
-            alert("Remis, Stalemate!");
-            window.location.href = "https://stejproject.github.io/openproject/belajar.html";
-            setTimeout(function () {
-                  values = ['r', 'n', 'b', 'q', 'k', 'b', 'n', 'r', 'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 't', 'm', 'v', 'w', 'l', 'v', 'm', 't'];
-            }, 100);
         }
 
         var tmpA = [];
